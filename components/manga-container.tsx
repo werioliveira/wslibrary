5e53178abcded0aa3ef73c63786b9ee4136b7da6
Weri Oliveira <werioliveira@hotmail.com>
2024-11-30 22:25:28 -0300

    fix page dependendy from useEffect and remove duplicate useeffect

--- components/manga-container.tsx
+++ components/manga-container.tsx
@@ -30,10 +30,8 @@ export function MangaContainer({ status, page, searchName }: { status: string; p
 
   useEffect(() => {
     setCurrentPage(page);
-  }, [status]);
-  useEffect(() => {
-    setCurrentPage(page);
-  }, [searchName]);
+  }, [status, page, searchName]);
+  
   if (isLoading)
     return (
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3">
