0278689eff05999d3e5bd49e1c8c09b7bccb2088
Weri Oliveira <werioliveira@hotmail.com>
2024-12-08 10:16:44 -0300

    add slimeread origin to try fetch data

--- lib/fetchManga.ts
+++ lib/fetchManga.ts
@@ -16,6 +16,7 @@ export async function fetchMangasFromSite(url: string, parseFunction: (data: str
   const { data } = await axios.get(url, {
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      'Origin': 'https://slimeread.com'
     },
   });
 
