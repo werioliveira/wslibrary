ff71d57114d2265186a683fc80d6b011bee5c8fc
Weri Oliveira <werioliveira@hotmail.com>
2024-12-08 16:15:45 -0300

    adjust axios from fetchmangas

--- lib/fetchManga.ts
+++ lib/fetchManga.ts
@@ -13,11 +13,7 @@ interface ScrapedManga {
 }
 // Função genérica para buscar dados de um site
 export async function fetchMangasFromSite(url: string, parseFunction: (data: string) => ScrapedManga[], source: string): Promise<ScrapedManga[]> {
-  const { data } = await axios.get(url, {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    },
-  });
+  const { data } = await axios.get(url);
 
   const mangas = parseFunction(data);
   
