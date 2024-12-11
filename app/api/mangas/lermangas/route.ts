430637af1f4ec6bec77273492cbec913c4a8b5a2
Weri Oliveira <werioliveira@hotmail.com>
2024-12-10 23:22:07 -0300

    fix lint

--- app/api/mangas/lermangas/route.ts
+++ app/api/mangas/lermangas/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { fetchMangasFromSite, parseLerMangas } from "@/lib/fetchManga";
+
 import puppeteer from 'puppeteer';
 // Tipo para o ScrapedManga
 type ScrapedManga = {
