7dfe4b3605d36a95160905e3d66f1a6b0cd48a22
Weri Oliveira <werioliveira@hotmail.com>
2025-05-12 15:43:41 +0000

    try to update with adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8450170649404542"
+          crossOrigin="anonymous"></script>        
+      </head>
       <body className={inter.className + " bg-zinc-950 text-white min-h-screen"}>
         <Providers>
           <Navbar  />
